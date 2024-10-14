8dcabaa2030d582d9520f8a39c5f424c3bd9bf16
Jishan2610 <jishan4all@gmail.com>
2024-10-14 13:35:34 +0530

    Socket code added for data member change on client side

--- SocketHandlers/dataMemberChangeHandler.js
+++ SocketHandlers/dataMemberChangeHandler.js
@@ -1,11 +1,19 @@
 function dataMemberChangeHandler(io, socket) {
     // Code change
-    socket.on('code change', ({room , code}) => {
-        socket.to(room).emit('code change', {
-            code: code,
+    socket.on('code change', ({roomId , newCode}) => {
+      console.log(roomId+" room in socket "+newCode+' code in socket')
+        socket.to(roomId).emit('code changed', {
+            code: newCode,
             senderId: socket.id,
           });
     });
+    socket.on('language change', ({roomId ,newLanguage, code}) => {
+      socket.to(roomId).emit('language changed', {
+          newLanguage,
+          code,
+          senderId: socket.id,
+        });
+  });
   
     
   }
