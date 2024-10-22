7e177a9185b8d3471af3657fb792544a3581cf61
Jishan2610 <jishan4all@gmail.com>
2024-10-23 03:12:49 +0530

    Added getUserSPecificRooms route with controller

--- Routes/room.js
+++ Routes/room.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const {authMiddleware}=require("../Middlewares/authMiddleware")
-const {createRoom}=require("../Controllers/room")
+const {createRoom,getUserSpecificRooms}=require("../Controllers/room");
+const { getUser } = require('../Controllers/user');
 
 const router=express.Router();
 
 ////<<--------------Room creation-------------->>
 router.post("/create",authMiddleware,createRoom)
-
+////<<--------------Get User Specific Rooms-------------->>
+router.get("/getRooms",authMiddleware,getUserSpecificRooms)
 
 module.exports=router
 
